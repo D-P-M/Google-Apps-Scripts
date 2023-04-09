7223a9ec6a8f51b115761f31ddea0d212a501f27
DPM <dylan.phillip.meyers@gmail.com>
2023-04-08 17:42:03 -0700

    add spreadsheet tab reset and email pdf folder

---
+++ spreadsheet_resetTabColors.js
@@ -0,0 +1,8 @@
+function resetTabColors() {
+    var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    var sheets = spreadsheet.getSheets();
+    for (var i = 0; i < sheets.length; i++) {
+      var sheet = sheets[i];
+      sheet.setTabColor(null);
+    }
+  }
